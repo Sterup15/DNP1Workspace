890350a096c1e36d4a6570af8ab22e83413564dc
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-09-13 09:04:08 +0200

    Made project

---
+++ 2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<int, int, int> add = (a, b) => a + b;
+        Func<int, int, int> multiply = (a, b) => a * b;
+
+        int num1 = int.Parse(Console.ReadLine()!);
+        int num2 = int.Parse(Console.ReadLine()!);
+        
+        Console.WriteLine(add(num1, num2));
+        Console.WriteLine(multiply(num1, num2));
+        
+    }
+}
